629a0770e07529648c0f9eb54c3346157e3b1c25
DEX406 <jakelutz1995@gmail.com>
2026-04-07 17:31:08 +0930

    Update TextRenderer.js

--- src/webgl/TextRenderer.js
+++ src/webgl/TextRenderer.js
@@ -54,7 +54,7 @@ export class TextRenderer {
 
   _render(item) {
     const gl = this.gl;
-    const scale = 4;
+    const scale = 5;
     const w = Math.ceil(item.w * scale);
     const h = Math.ceil(item.h * scale);
 
